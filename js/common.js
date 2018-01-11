eb836e5441c2831d92dfef1a7a5ed135a1154e79
Aleksandr-si <ises2000@mail.ru>
2018-01-11 15:11:41 +0200

    index

--- js/common.js
+++ js/common.js
@@ -3,5 +3,24 @@ $(".menu_ul>li").hover(function(){
     $s_width=$(this).innerWidth();
     $(".hidden_menu>li").innerWidth($s_width);
 });
-
 });
+$(window).scroll(function() {
+    $('.mov').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+700) {
+        $(this).addClass('fadeInUp');
+        $(".master_work").css("opacity", "1");
+      }
+    });
+  });
+$(window).scroll(function() {
+    $('.mov1').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+700) {
+        $(this).addClass('fadeInUp');
+        $(".futures").css("opacity", "1");
+      }
+    });
+  });
